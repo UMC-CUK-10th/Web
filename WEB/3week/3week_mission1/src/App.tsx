275045001647b8063490cf58f8b8261105f3c1f7
김재범 <process68@naver.com>
2026-03-30 20:30:47 +0900

    3주차 미션2 완료

--- WEB/3week/3week_mission1/src/App.tsx
+++ WEB/3week/3week_mission1/src/App.tsx
@@ -1,12 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import MoviesPage from './pages/MoviesPage'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element: <HomePage/>,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: 'movies/:category',
+        element: <MoviesPage />,
+        index: true,
+      },
+
+    ]
+  },
+])
 
 function App() {
 
   return (
-    <>
-      <MoviesPage />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
