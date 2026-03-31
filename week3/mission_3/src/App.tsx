789256c5f1cf067f3ad58cca48d63771ef64f705
woohyun <newoohyun1234@naver.com>
2026-04-01 04:19:53 +0900

    week_3

---
+++ week3/mission_3/src/App.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import HomePage from "./pages/HomePage";
+import PopularPage from "./pages/PopularPage";
+import UpcomingPage from "./pages/UpcomingPage";
+import TopRatedPage from "./pages/TopRatedPage";
+import NowPlayingPage from "./pages/NowPlayingPage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "popular", element: <PopularPage /> },
+      { path: "upcoming", element: <UpcomingPage /> },
+      { path: "top-rated", element: <TopRatedPage /> },
+      { path: "now-playing", element: <NowPlayingPage /> },
+      { path: "movies/:movieId", element: <MovieDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
