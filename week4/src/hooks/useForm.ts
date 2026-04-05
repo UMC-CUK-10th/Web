54df8da4210dc91257e6b90c50cdf8b03bc97c17
ysys3535 <yoonseo3139@gmail.com>
2026-04-05 23:22:58 +0900

    mission2

---
+++ week4/src/hooks/useForm.ts
@@ -0,0 +1,41 @@
+import { useState, type ChangeEvent } from "react";
+
+
+interface UseFormProps<T> {
+    initialValue: T,
+    validate: (values: T) => Record<keyof T, string>;
+}
+
+function useForm<T> ({initialValue, validate}: UseFormProps<T>) {
+    const [values, setValues] = useState<T>(initialValue);
+    const [touched, setTouched] = useState<Record<string, boolean>>( {} );
+    const errors = validate(values) as Record<string, string>;
+
+    const handleChange = (name: keyof T, text: string) => {
+        setValues({
+            ...values, 
+            [name]: text,
+        });
+    };
+
+    const handleBlur = (name: keyof T) => {
+        setTouched({
+            ...touched,
+            [name]: true,
+        })
+    }
+
+    const getInputProps = (name: keyof T) => {
+        const value = values[name];
+
+        const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        ) => handleChange(name, e.target.value);
+
+        const onBlur = () => handleBlur(name);
+
+        return {value, onChange, onBlur };
+    };
+    return { values, errors, touched, getInputProps };
+}
+
+export default useForm;
