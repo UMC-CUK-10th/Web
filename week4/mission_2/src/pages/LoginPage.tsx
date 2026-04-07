fc46211ad841cb1221d6ae7a1617c872301d179d
woohyun <newoohyun1234@naver.com>
2026-04-07 20:03:30 +0900

    mission_2

---
+++ week4/mission_2/src/pages/LoginPage.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import useForm from "../hooks/useForm";
+import { validateSignin, type UserSigninInformation } from "./utils/validate";
+
+const LoginPage = () => {
+  const { values, errors, touched, getInputProps } =
+    useForm<UserSigninInformation>({
+      initialValue: {
+        email: "",
+        password: "",
+      },
+      validate: validateSignin,
+    });
+
+  const handleSubmit = async () => {
+    
+  };
+
+  const isDisabled: boolean =
+    Object.values(errors || {}).some((error) => error.length > 0) ||
+    Object.values(values).some((value) => value === "");
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-4">
+      <div className="flex flex-col gap-3">
+        <input
+          {...getInputProps("email")}
+          type="email"
+          className="border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm"
+          placeholder="이메일"
+        />
+        {errors?.email && touched?.email && (
+          <div className="text-red-500 text-sm">{errors.email}</div>
+        )}
+
+        <input
+          {...getInputProps("password")}
+          type="password"
+          className="border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm"
+          placeholder="비밀번호"
+        />
+        {errors?.password && touched?.password && (
+          <div className="text-red-500 text-sm">{errors.password}</div>
+        )}
+
+        <button
+          type="button"
+          onClick={handleSubmit}
+          disabled={isDisabled}
+          className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
