e0c00313bca0a3440a891d923c1feb5d072be225
이주은 <joon@ijueun-ui-MacBookAir.local>
2026-04-28 23:01:30 +0900

    5주차 미션2

--- 2week-mission2/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import ContextPage from './ContextPage';
-
-function App() {
-  return (
-    <ContextPage />
-  );
-}
-
-export default App;
--- 3week_SPA/src/Route.tsx
+++
@@ -1,8 +0,0 @@
-interface RouteProps {
-  path: string;
-  component: React.ComponentType<any>;
-}
-
-export const Route = ({ component: Component } : RouteProps) => {
-  return <Component/>
-};
