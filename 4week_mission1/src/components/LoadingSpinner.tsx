40a6c1832a18e7a4c4b210351211d14efdf32a9f
이주은 <joon@ijueun-ui-MacBookAir.local>
2026-05-16 15:45:24 +0900

    8주차 미션1

--- 4week_mission1/src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export const LoadingSpinner = () => {
-  return (
-    <div className='size-12 animate-spin rounded-full border-6 border-t-transparent border-gray-300' role='status'>
-      <span className='sr-only'>로딩 중</span>
-    </div>
-  )
-}
