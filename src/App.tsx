6531ea2dda4c00f037a34a9aee90652299d0fc85
m0jujukim-cyber <m0jujukim@gmail.com>
2026-05-19 20:35:38 +0900

    w8m2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import LpDetailPage from './pages/LpDetailPage';
+import ThrottlePage from './pages/ThrottlePage';
 
 // 1. 홈페이지
 // 2. 로그인 페이지
@@ -33,6 +34,7 @@ const publicRoutes: RouteObject[] = [
       { path: 'signup', element: <SignupPage /> },
       { path: 'v1/auth/google/callback', element: <GoogleLoginRedirectPage /> },
       { path: 'lps/:lpId', element: <LpDetailPage /> },
+      { path: '/throttle', element: <ThrottlePage /> },
     ],
   },
 ];
