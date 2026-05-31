d60262e8546dc2cdd001de13733020aad9cff051
wonjun6768-dev <wonjun6768@gmail.com>
2026-05-31 14:44:28 +0900

    9주차 미션1

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
 )
->>>>>>> upstream/체컵/고원준
